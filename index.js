d8b9f59073064ab3531be027f028aea492051cc1
nour khoury <khourynour79@gmail.com>
2022-09-13 23:33:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(height , width,area) {
+    //returns the number of blocks given a value
+
+    return (height*width)/area;
+  }
+
+
+  function distanceFromHqInFeet(distance,time,feet) {
+
+
+    return distanceFromHqInBlocks(distance,time,feet);
+       
+
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+
+     return numFeet = start *destination;
+
+  }
+
+  function calculatesFarePrice(start, destination) {
+    
+    return price = (start + destination )/ 2;
+  }
